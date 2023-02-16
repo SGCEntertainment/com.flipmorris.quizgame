782538bf77506c253041dfb487ac9e424061f2b7
SGCEntertainment <smallgamescompany@gmail.com>
2023-02-16 14:03:37 +0300

    update

--- Assets/com.flipmorris.quizgame/Scripts/Managers/GameManager.cs
+++ Assets/com.flipmorris.quizgame/Scripts/Managers/GameManager.cs
@@ -3,5 +3,58 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    
+    [SerializeField] GameObject menu;
+    [SerializeField] GameObject game;
+    [SerializeField] GameObject result;
+
+    [Space(10)]
+    [SerializeField] Text quizName;
+    [SerializeField] Text subjectText;
+    [SerializeField] Text chapterText;
+
+    [Space(10)]
+    [SerializeField] Text totalQuestionsText;
+    [SerializeField] Text totalTimeText;
+    [SerializeField] Text duplicateTimerText;
+
+    [Space(10)]
+    [SerializeField] Text instructionsText;
+
+    [Space(10)]
+    [SerializeField] QuestionData QuestionData;
+
+    private void Start() => Init();
+
+    private void Init()
+    {
+        quizName.text = QuestionData.quizName;
+        subjectText.text = QuestionData.subject;
+        chapterText.text = QuestionData.chapter;
+
+        totalQuestionsText.text = $"Total Questions: <b>{QuestionData.totalQuestionCount}</b>";
+        totalTimeText.text = $"Total Time: <b>{QuestionData.totalTimeCount} min</b>";
+
+        duplicateTimerText.text = $"{QuestionData.totalTimeCount}:00";
+        instructionsText.text = QuestionData.instructions;
+
+        game.SetActive(false);
+        result.SetActive(false);
+        menu.SetActive(true);
+    }
+
+    public void ExitGame() => Application.Quit();
+
+    public void StartGame()
+    {
+        result.SetActive(false);
+        menu.SetActive(false);
+
+        game.SetActive(true);
+    }
+
+    public void OpenMenu()
+    {
+        game.SetActive(false);
+        menu.SetActive(true);
+    }
 }
