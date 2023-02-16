f82d230317d4c313ecd0cdc3daf01a392ec20fe1
SGCEntertainment <smallgamescompany@gmail.com>
2023-02-16 13:48:44 +0300

    update

---
+++ Assets/com.flipmorris.quizgame/Scripts/Question.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Question
+{
+    public string question;
+
+    [Space(10)]
+    public string a;
+    public string b;
+    public string c;
+    public string d;
+
+    [Space(10)]
+    public string ansver;
+}
---
+++ Assets/com.flipmorris.quizgame/Scripts/ScriptableObjects/QuestionData.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class QuestionData : ScriptableObject
+{
+    public string quizName;
+
+    [Space(10)]
+    public string subject;
+    public string chapter;
+
+    [Space(10)]
+    public int totalQuestionCount;
+    public float totalTimeCount;
+
+    [Space(10), TextArea]
+    public string instructions;
+
+    [Space(10)]
+    public Question[] questions;
+}
